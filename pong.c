81e3d84e5b51b3b577827e7b5a4c3ceb9cb78df9
Paul Fridman <paulfridman7@gmail.com>
2026-02-06 20:41:45 -0600

    submission

--- pong.c
+++ pong.c
@@ -1,6 +1,6 @@
 /*
  * pong.c - UDP ping/pong server code
- *          author: <your name>
+ *          author: Paul Fridman
  */
 #include <netdb.h>
 #include <stdio.h>
@@ -38,6 +38,65 @@ int main(int argc, char **argv) {
   // pong implementation goes here.
   printf("nping: %d pongport: %s\n", nping, pongport);
 
+  int sockfd = -1;
+  struct addrinfo hints, *servinfo, *p;
+  int rv, yes;
+
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if((rv = getaddrinfo(NULL, pongport, &hints, &servinfo)) != 0){
+    perror("getaddrinfo");
+    return 1;
+  }
+
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+        perror("server: socket");
+        continue;
+    }
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+            sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        close(sockfd);
+        perror("server: bind");
+        continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo);
+
+  struct sockaddr_storage from_addr;
+
+  if(p == NULL){
+    perror("server failed bind\n");
+    close(sockfd);
+    exit(1);
+  }
+  char buf[2000];
+  char s[INET_ADDRSTRLEN];
+  socklen_t from_len = sizeof(from_addr);
+  for(int i = 0; i < nping; i++){
+    ssize_t arraysize = recvfrom(sockfd, buf, 2000, 0, 
+                      (struct sockaddr *) &from_addr, &from_len);
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)&from_addr;
+    inet_ntop(AF_INET, &(ipv4->sin_addr), s, sizeof s);
+    printf("pong[%d]: received packet from %s\n", i, s);
+    for(int el = 0; el < arraysize; el++){
+      buf[el] += 1;
+    }
+    sendto(sockfd, buf, arraysize, 0, (struct sockaddr *) &from_addr, from_len);
+  }
+  
   return 0;
 }
 
